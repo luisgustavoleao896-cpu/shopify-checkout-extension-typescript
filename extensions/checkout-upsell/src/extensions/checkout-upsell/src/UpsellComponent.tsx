353422869c19d78264f85121889a5050ea24a420
luisgustavoleao896-cpu <luisgustavoleao896@gmail.com>
2026-03-02 16:06:48 -0300

    Add UpsellComponent with promotional banner

---
+++ extensions/checkout-upsell/src/extensions/checkout-upsell/src/UpsellComponent.tsx
@@ -0,0 +1,9 @@
+import { Banner } from "@shopify/ui-extensions-react/checkout";
+
+export function UpsellComponent() {
+  return (
+    <Banner title="Produto complementar" status="success">
+      Aproveite esta oferta exclusiva no checkout.
+    </Banner>
+  );
+}
